cf474937ce91231d2cb5ac232f24493010fda5f2
Black-Loli <pundanya@gmail.com>
2022-10-18 00:10:17 +0300

    Мелкие правки ненужных вещей

--- src/js/photo_magnification.js
+++ src/js/photo_magnification.js
@@ -2,6 +2,5 @@ var gallery = document.querySelector('#gallery');
 gallery.querySelectorAll('.content').forEach(function (item) {
     item.addEventListener('click', function () {
         item.classList.toggle('full');
-        showSlides(slideIndex);
     });
 });
