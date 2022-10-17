e28a027f1ad10392e7c56fbb2888c9a1557878e0
Black-Loli <pundanya@gmail.com>
2022-10-17 17:26:22 +0300

    Легкие правки

--- photo_magnification.js
+++
@@ -1,7 +0,0 @@
-var gallery = document.querySelector('#gallery');
-gallery.querySelectorAll('.content').forEach(function (item) {
-    item.addEventListener('click', function () {
-        item.classList.toggle('full');
-        showSlides(slideIndex);
-    });
-});
