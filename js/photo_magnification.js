4fafcada7f28af8539e1bce2a1dca4a8ccb7e672
Black-Loli <pundanya@gmail.com>
2022-11-09 12:13:29 +0300

    Чёт поменялось, но что непонятно

--- js/photo_magnification.js
+++ js/photo_magnification.js
@@ -1,9 +1,10 @@
-var gallery = document.querySelector('#gallery');
-gallery.querySelectorAll('.content').forEach(function (item) {
-    item.addEventListener('click', () => {
-        item.classList.toggle('full');
-        showSlides(slideIndex);
-    });
+var gallery = document.querySelector("#gallery");
+gallery.querySelectorAll(".content").forEach(function (item) {
+	item.addEventListener("click", () => {
+		item.classList.toggle("full");
+	});
 });
 
 console.log("Акитушки");
+console.error("Пропали Акитушки");
+console.warn("АКИТУШКИ ВСЁ ТАК ЖЕ ПРОПАЛИ, ИЩЕМ ИХ");
